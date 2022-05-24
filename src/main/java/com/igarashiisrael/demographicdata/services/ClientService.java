b69fc7a48c49358b2c476f19940f64cfda35e433
Israel <igarashiisrael@icloud.com>
2022-05-24 15:18:56 -0300

    Database seedind

--- src/main/java/com/igarashiisrael/demographicdata/services/ClientService.java
+++ src/main/java/com/igarashiisrael/demographicdata/services/ClientService.java
@@ -4,6 +4,7 @@ import com.igarashiisrael.demographicdata.entities.Client;
 import com.igarashiisrael.demographicdata.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Client> findAll(){
         return repository.findAll();
     }
